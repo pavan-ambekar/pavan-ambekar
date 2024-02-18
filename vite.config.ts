f2d997508772ddf93f36de9e9c6ffcfb7838a1cc
Pavan A <pavan479ec@gmail.com>
2024-02-19 00:10:06 +0530

    fix: updated url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: 'https://pavan-ambekar.github.io/',
+    base: 'https://pavan-ambekar.github.io/pavan-ambekar/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
